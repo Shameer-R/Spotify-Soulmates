c0817529fd3cc885cbe6ef6da801fdd7c7f378b4
Shameer-R <roseshammy@gmail.com>
2024-02-14 23:25:41 -0500

    Implement groundwork for getting access token

--- server/index.js
+++ server/index.js
@@ -1,3 +1,26 @@
-require("dotenv").config();
+require("dotenv").config({path: "./.env"});
+var request = require("request");
 
-console.log(process.env.API_KEY)
+
+const client_id = process.env.ClientID;
+const client_secret = process.env.ClientSecret;
+
+var authOptions = {
+    url: 'https://accounts.spotify.com/api/token',
+    headers: {
+        'Authorization': 'Basic ' + (new Buffer.from(client_id + ':' + client_secret).toString('base64'))
+    },
+    form: {
+        grant_type: 'client_credentials'
+    },
+    json: true
+};
+
+request.post(authOptions, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+        var token = body.access_token;
+        console.log(token);
+    } else {
+        console.log(response.statusCode);
+    }
+});
