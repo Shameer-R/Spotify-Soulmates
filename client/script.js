73a3599a0d1521cde1f307fa2cfcf6f84bea432d
Shameer-R <roseshammy@gmail.com>
2024-05-10 23:40:47 -0400

    Fetch blank data from server

--- client/script.js
+++ client/script.js
@@ -1,3 +1,22 @@
 const user_1_input = document.getElementById("user-1-input")
 const user_2_input = document.getElementById("user-2-input")
 const submitButton = document.getElementById("submit")
+
+submitButton.addEventListener("click", async () => {
+    
+    user1_ID = user_1_input.value 
+    user2_ID = user_2_input.value
+
+    const url = `http://localhost:3000/compatibility/${user1_ID}/${user2_ID}`
+
+    try {
+        fetch(url)
+        .then(response => response.json)
+        .then(data => {
+            console.log(data)
+        })
+    } catch (error) {
+        console.error(error)
+    }
+
+})
